1126e14dba166dad39cb3150aceefe7c172aee16
Asia Antczak <aasiaantczak@gmail.com>
2018-05-18 16:10:37 +0100

    weather is working with geolocation

--- src/api-interface.js
+++ src/api-interface.js
@@ -1,25 +1,21 @@
+navigator.geolocation.getCurrentPosition(function(position) {
 
-navigator.geolocation.getCurrentPosition(function(position){
-  return (position.coords.latitude, position.coords.longitude)
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather",
 
-$.ajax({
-  url: "https://api.openweathermap.org/data/2.5/weather",
+    data: {
+        lon: position.coords.longitude,
+        lat: position.coords.latitude,
+        APPID: '640f7b83a809dee5f3bdadae5d2603ab'
+    },
 
-  data: {
-      lon: position.coords.longitude,
-      lat: position.coords.latitude,
-      units: 'metric',
-    APPID: '640f7b83a809dee5f3bdadae5d2603ab'
-  },
+    type: "GET",
 
-  type: "GET",
+    dataType: "json",
 
-  dataType: "json",
-
-})
-.done (function( json) {
-  console.log(position.coords.longitude)
-  $("<span>").text( (json.name) + "  " ).appendTo( ".screen" );
-  $("<span>").text( (Math.round(json.main.temp - 273.15))+String.fromCharCode(176)).appendTo( ".screen" );
-})
+  })
+  .done (function( json) {
+    $("<span>").text( (json.name) + "  " ).appendTo( ".screen" );
+    $("<span>").text( (Math.round(json.main.temp - 273.15))+String.fromCharCode(176)).appendTo( ".screen" );
+  })
 });
